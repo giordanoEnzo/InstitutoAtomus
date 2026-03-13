ee10f7e9b05d104ed28f18cd900591eb8d97a8f1
giordanoenzo <enzogiordanoofficial@gmail.com>
2026-03-13 14:40:31 -0300

    correcao noticias

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,6 +1,8 @@
 import Link from 'next/link';
 import AdminSidebar from '@/components/AdminSidebar';
 
+export const dynamic = 'force-dynamic';
+
 export const metadata = {
     title: 'Painel Administrativo | ATOMUS',
 };
--- src/app/biblioteca/page.tsx
+++ src/app/biblioteca/page.tsx
@@ -1,5 +1,7 @@
 import prisma from '@/lib/prisma';
 import Link from 'next/link';
+
+export const dynamic = 'force-dynamic';
 import FooterMain from '@/components/FooterMain';
 
 export const metadata = {
